0818d5edd38de236fa6986eb40d7151b3cec3d49
Dylan-Greathouse <Greathouse.dylanj@gmail.com>
2021-08-10 15:38:36 -0700

    added more to data set

--- data.js
+++ data.js
@@ -1,10 +1,50 @@
 const data = [
     {
-        id: 1
+        id: 1,
+        name: `Demon's Souls`,
+        system: `PlayStation 3`,
+        played: true,
+
+    },
+    
+    {
+        id: 2,
+        name: `Demon's Souls II`,
+        system: `PlayStation 3`,
+        played: true,
         
     },
+    
     {
+        id: 3,
+        name: `Demon's Souls III`,
+        system: `PlayStation 3`,
+        played: true,
+        
+    },
+    
+    {
+        id: 4,
+        name: `Bloodborne`,
+        system: `PlayStation 4`,
+        played: false,
+        
+    },
 
+    {
+        id: 5,
+        name: `Sekiro: Shadows Die Twice`,
+        system: `PlayStation 4`,
+        played: true,
+        
+    },
+
+    {
+        id: 6,
+        name: `Elden Ring`,
+        system: `PlayStation 5`,
+        played: false,
+        
     },
 ];
 //exports are a little different than in Node
