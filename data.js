c788a9695114003ed01b4bdd4cfc809f448b7484
Dylan-Greathouse <Greathouse.dylanj@gmail.com>
2021-08-10 16:21:32 -0700

    changing data structure

--- data.js
+++ data.js
@@ -1,49 +1,49 @@
 const data = [
     {
-        "_id": 1,
-        "name": "Demon's-Souls",
-        "system": "PlayStation3",
-        "played": true,
+        id: 1,
+        name: "Demon's-Souls",
+        system: "PlayStation3",
+        played: true,
 
     },
     
     {
-        "_id": 2,
+        id: 2,
         name: "Demon's-Souls-II",
         system: "PlayStation3",
-        "played": true,
+        played: true,
         
     },
     
     {
-        "_id": 3,
-        "name": "Demon's-Souls-III",
-        "system": "PlayStation3",
-        "played": true,
+        id: 3,
+        name: "Demon's-Souls-III",
+        system: "PlayStation3",
+        played: true,
         
     },
     
     {
-        "_id": 4,
-        "name": "Bloodborne",
-        "system": "PlayStation4",
-        "played": false,
+        id: 4,
+        name: "Bloodborne",
+        system: "PlayStation4",
+        played: false,
         
     },
 
     {
-        "_id": 5,
-        "name": "Sekiro",
-        "system": "PlayStation 4",
-        "played": true,
+        id: 5,
+        name: "Sekiro",
+        system: "PlayStation 4",
+        played: true,
         
     },
 
     {
-        "_id": 6,
-        "name": "Elden-Ring",
-        "system": "PlayStation5",
-        "played": false,
+        id: 6,
+        name: "Elden-Ring",
+        system: "PlayStation5",
+        played: false,
         
     },
 ];
